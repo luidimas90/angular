6e251decfef71f93718250c41d8d13afed439d27
unknown <nathalyadriana@gmail.com>
2020-03-14 22:46:09 -0600

    Added recipe list and details

--- recipes-app/src/app/recipes/recipes-list/recipes-list.component.ts
+++ recipes-app/src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipes-list',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipes-list.component.css']
 })
 export class RecipesListComponent implements OnInit {
+
+  recipes : Recipe [] = [
+    new Recipe("Recipe1", "This is the first recipe","https://www.onceuponachef.com/images/2017/12/NY-Cheesecake-760x574.jpg"),
+    new Recipe("Recipe1", "This is the first recipe","https://www.onceuponachef.com/images/2017/12/NY-Cheesecake-760x574.jpg")
+  ];
 
   constructor() { }
 
